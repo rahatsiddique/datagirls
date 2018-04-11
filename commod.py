2755b8236cffef2f512f029da9036fcfc479cb6a
Maria <mtsarkova@yahoo.com>
2018-04-11 19:27:49 +0100

    imported html css, needs changes

--- commod.py
+++ commod.py
@@ -1,26 +1,26 @@
 import StringIO
 import base64
 
+
 import quandl
 
 def get_commod_data():
     quandl.ApiConfig.api_key = '3tWQBLpyDZsoV-TxKWH8'
     quandl.ApiConfig.api_version = '2015-04-09'
 
-    data = quandl.get('NSE/OIL')
+    data =quandl.get('NSE/OIL')
 
     # data = quandl.get_table('ZACKS/FC', ticker='AAPL')
 
     return data
 
-def chart ():
+def chart():
     x = data["date"].tolist()
     y = data["high"].tolist()
-    return render_template('chart.html', y=y, x=x)
+    return render_template('commodities.html', y=y, x=x)
 
 data = get_commod_data()
-col1 = get_col_1()
+col1 = get_col1()
 col2 = get_col2()
 
-print(col1)
-print(col2)
+
--- mc.py
+++ mc.py
@@ -2,6 +2,9 @@ import quandl
 quandl.ApiConfig.api_key = 'qe_zYg_VZHnH5SEzVML_'
 quandl.ApiConfig.api_version = '2015-04-09'
 
-data=quandl.get('NSE/OIL')
+data=quandl.get('LME/PR_CU')
 
 print data
+
+import quandl
+# data = quandl.get_table('ZACKS/FC', ticker='AAPL')
