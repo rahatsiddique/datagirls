373a4cc32684081b415d3702ffc31b99126697dc
Maria <mtsarkova@yahoo.com>
2018-04-17 18:17:09 +0100

    we have one graph for gold. need to think how to make multiple graphs and change the date outlook. need to change the whole content.

--- commod.py
+++ commod.py
@@ -1,5 +1,4 @@
-import StringIO
-import base64
+
 
 
 import quandl
@@ -9,6 +8,26 @@ def get_commod_data():
     quandl.ApiConfig.api_version = '2015-04-09'
 
     data = quandl.get('WGEC/WLD_GOLD')
+    #data1 = quandl.get( 'WGEC/WLD_WOODPULP' )
+    #data2 = quandl.get( 'WGEC/WLD_SILVER' )
+    #data3 = quandl.get( 'WGEC/WLD_NICKEL' )
         # data = quandl.get_table('ZACKS/FC', ticker='AAPL')
-    print data
+    dataDict = data.to_dict('split')
+    newdict = {1: 0, 2: 0, 3: 0}
+    #print dataDict.values()
+    timestampsList = dataDict["index"]
 
+    dataList = dataDict["data"]
+    #time_str=str(timestampsList)
+    #year=time_str[:4]
+    #timelessList = [i.split()[0] for i in timestampsList]
+
+    print timestampsList.date
+    print dataList
+    #print timelessList
+    #print year
+    #print data1
+    #print data2
+    #print data3
+
+get_commod_data()
--- page.py
+++ page.py
@@ -5,14 +5,15 @@ Bootstrap(app)
 
 import quandl
 
-def get_commod_data():
+def get_commod_data(getcmd):
     quandl.ApiConfig.api_key = '3tWQBLpyDZsoV-TxKWH8'
     quandl.ApiConfig.api_version = '2015-04-09'
 
-    data = quandl.get('WGEC/WLD_GOLD')
-    data1 = quandl.get('WGEC/WLD_WOODPULP')
-    data2 = quandl.get('WGEC/WLD_SILVER')
-    data3 = quandl.get('WGEC/WLD_NICKEL')
+    #data = quandl.get('WGEC/WLD_GOLD')
+    #data1 = quandl.get('WGEC/WLD_WOODPULP')
+    #data2 = quandl.get('WGEC/WLD_SILVER')
+    #data3 = quandl.get('WGEC/WLD_NICKEL')
+    data = quandl.get(getcmd)
     return data
 
 @app.route('/')
@@ -30,12 +31,25 @@ def commodities():
 
 @app.route ('/graph')
 def graph():
-    data = get_commod_data()
+    # data = quandl.get('WGEC/WLD_GOLD')
+    # data1 = quandl.get('WGEC/WLD_WOODPULP')
+    # data2 = quandl.get('WGEC/WLD_SILVER')
+    # data3 = quandl.get('WGEC/WLD_NICKEL')
+    data = get_commod_data('WGEC/WLD_GOLD')
+    #data1 = get_commod_data( 'WGEC/WLD_WOODPULP' )
+    #data2 = get_commod_data( 'WGEC/WLD_SILVER' )
+    #data3 = get_commod_data( 'WGEC/WLD_NICKEL' )
 
-    col_1 = data["Value"].tolist()
-    col_2 = range(len(col_1))
+    dataDict = data.to_dict( 'split' )
 
-    return render_template( "graph.html", labels=col_2, values=col_1, title='Gold price', arrays="numpy", paginate=True, min=20, max=1000)
+    timestampsList = dataDict["index"]
+    dataList = dataDict["data"]
+
+    print dataList
+    print timestampsList
+
+    #col_2 = range(len(col_1))
+    return render_template( "graph.html",labels=timestampsList, values=dataList, title='Gold price', paginate=True,min=20, max=1700)
 
 if __name__== "__main__":
     app.run(debug=True)
