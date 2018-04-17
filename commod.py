28bcd31cd3ebe5e2992a2a2acfbe28962d49d9bd
Maria <mtsarkova@yahoo.com>
2018-04-17 11:52:03 +0100

    worked on charts, added some data on commodities. we need to write about us and link html

--- commod.py
+++ commod.py
@@ -8,19 +8,7 @@ def get_commod_data():
     quandl.ApiConfig.api_key = '3tWQBLpyDZsoV-TxKWH8'
     quandl.ApiConfig.api_version = '2015-04-09'
 
-    data =quandl.get('NSE/OIL')
-
-    # data = quandl.get_table('ZACKS/FC', ticker='AAPL')
-
-    return data
-
-def chart():
-    x = data["date"].tolist()
-    y = data["high"].tolist()
-    return render_template('commodities.html', y=y, x=x)
-
-data = get_commod_data()
-col1 = get_col1()
-col2 = get_col2()
-
+    data = quandl.get('WGEC/WLD_GOLD')
+        # data = quandl.get_table('ZACKS/FC', ticker='AAPL')
+    print data
 
--- page.py
+++ page.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-app= Flask (__name__)
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+Bootstrap(app)
 
 import quandl
 
@@ -7,31 +9,33 @@ def get_commod_data():
     quandl.ApiConfig.api_key = '3tWQBLpyDZsoV-TxKWH8'
     quandl.ApiConfig.api_version = '2015-04-09'
 
-    data = quandl.get('FRED/SIPOVGINIDEU')
+    data = quandl.get('WGEC/WLD_GOLD')
+    data1 = quandl.get('WGEC/WLD_WOODPULP')
+    data2 = quandl.get('WGEC/WLD_SILVER')
+    data3 = quandl.get('WGEC/WLD_NICKEL')
     return data
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template ("About_us.html")
+
 @app.route ('/commodities')
 def commodities():
     commodities = ["Oil" , "Steel", "Gold"]
     return render_template("commodities.html", commodities=commodities)
 
-@app.route ('/graphs')
-def chart():
+@app.route ('/graph')
+def graph():
     data = get_commod_data()
 
     col_1 = data["Value"].tolist()
     col_2 = range(len(col_1))
 
-    print col_1
-    print col_2
+    return render_template( "graph.html", labels=col_2, values=col_1, title='Gold price', arrays="numpy", paginate=True, min=20, max=1000)
 
-
-    return render_template("commodities.html", y=col_2, x=col_1)
-
-
-if __name__=="__main__":
-    app.run()
+if __name__== "__main__":
+    app.run(debug=True)
